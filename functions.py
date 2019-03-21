4cdaf408cb61fc376af511857ccbc951daee26ef
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-03-21 14:50:47 +0000

    Decompose load_vgg, layers, optimize, train_nn into separate module

---
+++ functions.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    # TODO: Implement function
+    #   Use tf.saved_model.loader.load to load the model and weights
+    vgg_tag = 'vgg16'
+    vgg_input_tensor_name = 'image_input:0'
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
+    
+    tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+    graph = tf.get_default_graph()
+        
+    names = (
+        vgg_input_tensor_name, 
+        vgg_keep_prob_tensor_name, 
+        vgg_layer3_out_tensor_name, 
+        vgg_layer4_out_tensor_name, 
+        vgg_layer7_out_tensor_name
+    )
+    
+    tensors = tuple(graph.get_tensor_by_name(name) for name in names)
+    return tensors
+
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    # TODO: Implement function
+    return None
+
+
+def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+    """
+    Build the TensorFLow loss and optimizer operations.
+    :param nn_last_layer: TF Tensor of the last layer in the neural network
+    :param correct_label: TF Placeholder for the correct label image
+    :param learning_rate: TF Placeholder for the learning rate
+    :param num_classes: Number of classes to classify
+    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    """
+    # TODO: Implement function
+    return None, None, None
+
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate):
+    """
+    Train neural network and print out the loss during training.
+    :param sess: TF Session
+    :param epochs: Number of epochs
+    :param batch_size: Batch size
+    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
+    :param train_op: TF Operation to train the neural network
+    :param cross_entropy_loss: TF Tensor for the amount of loss
+    :param input_image: TF Placeholder for input images
+    :param correct_label: TF Placeholder for label images
+    :param keep_prob: TF Placeholder for dropout keep probability
+    :param learning_rate: TF Placeholder for learning rate
+    """
+    # TODO: Implement function
+    pass
--- pg.py
+++ pg.py
@@ -1,51 +1,16 @@
 # Playground
 
 import tensorflow as tf
+from functions import load_vgg
 
 saved_model_dir = '/data/vgg/'
-
-def load_vgg(sess, vgg_path):
-    """
-    Load Pretrained VGG Model into TensorFlow.
-    :param sess: TensorFlow Session
-    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
-    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
-    """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
-    vgg_tag = 'vgg16'
-    vgg_input_tensor_name = 'image_input:0'
-    vgg_keep_prob_tensor_name = 'keep_prob:0'
-    vgg_layer3_out_tensor_name = 'layer3_out:0'
-    vgg_layer4_out_tensor_name = 'layer4_out:0'
-    vgg_layer7_out_tensor_name = 'layer7_out:0'
-        
-    with tf.Session() as sess:
-        tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
-        graph = tf.get_default_graph()
-        
-    names = (
-        vgg_input_tensor_name, 
-        vgg_keep_prob_tensor_name, 
-        vgg_layer3_out_tensor_name, 
-        vgg_layer4_out_tensor_name, 
-        vgg_layer7_out_tensor_name
-    )
-    
-    tensors = tuple(graph.get_tensor_by_name(name) for name in names)
-    return tensors
 
 if __name__ == '__main__':
     
     print('TensorFlow version:', tf.__version__)
     
-    
-    
-    """
     with tf.Session() as sess:
-        tf.saved_model.loader.load(sess, ['vgg16'], saved_model_dir)
-        graph = tf.get_default_graph()
-
-        #print(graph.get_operations())
+        tensors = load_vgg(sess, saved_model_dir)
         
-    """
+        
+    
